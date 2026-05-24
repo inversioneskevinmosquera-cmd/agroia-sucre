703cd9c077b551e9af853b3c4d5e8d7d5f8684cf
Kevin Mosquera <inversioneskevinmosquera@gmail.com>
2026-05-24 08:04:26 -0500

    Primera version AGROIA

---
+++ frontend/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-green-100">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-green-800">
+          AGROIA SUCRE
+        </h1>
+
+        <p className="mt-4 text-xl text-green-700">
+          Sistema inteligente de gestión hídrica
+        </p>
+      </div>
+    </main>
+  );
+}
